b33737fa0941d7b23354bbcb6a4277abde979f75
Tyreum <gabriel.kmn@gmail.com>
2022-06-05 21:44:31 -0300

    Generating questions

--- script.js
+++ script.js
@@ -111,30 +111,61 @@ const questions = [
     },
 ]
 
-function showQuestions(questions, quizContainer){
-    var output = [];
-    var answers;
+var quizContainer = document.getElementById('');
+var resultsContainer = document.getElementById('');
+var submitButton = document.getElementById('');
 
-    for (let i = 0; i < questions.length; i++) {
-        answers = [];
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
-        for (letter in questions[i].answers) {
-            answers.push(
-                '<label>'
-                    + '<input type="radio" name="question'+i+'value="'+letter+'"'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                + '</label>'
+function generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton){
+    function showQuestions(questions, quizContainer){
+        var output = [];
+        var answers;
+
+        for (let i = 0; i < questions.length; i++) {
+            answers = [];
+
+            for (letter in questions[i].answers) {
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'value="'+letter+'"'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
             );
         }
-        output.push(
-            '<div class="question">' + questions[i].question + '</div>'
-            + '<div class="answers">' + answers.join('') + '</div>'
-        );
+
+        quizContainer.innerHTML = output.join('');
     }
 
-    quizContainer.innerHTML = output.join('');
+    function showResults(questions, quizContainer, resultsContainer){
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        for (var i = 0; i < questions.length; i++) {
+            userAnswer = (answerContainers[i].querySelector('input[name = question'+ i +']:checked')||{}).value;
+            
+            if(userAnswer === questions[i].correctAnswer){
+                numCorrect++;
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            else{
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
 }
+
+// Transition
 
 function hide(){
     var element = document.getElementById("Card");
